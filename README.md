df65ab2fcd658083c2eebbaf95fc4fd76e743f78
Kevin Desigaux <k.desigaux@cpro.fr>
2019-02-18 13:11:22 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Vue Calendar
+
+This is a wild fork from [vue-fullcalendar](https://wanderxx.github.io/vue-fullcalendar/).
+
---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+const webpack = require('webpack');
+const path = require('path');
+const TerserPlugin = require('terser-webpack-plugin');
+const VueLoaderPlugin = require('vue-loader/lib/plugin');
+
+module.exports = {
+  entry: path.resolve(__dirname + '/src/Calendar.vue'),
+  output: {
+    path: path.resolve(__dirname + '/dist/'),
+    filename: 'vue-calendar.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: [
+          'vue-style-loader',
+          'css-loader',
+          'sass-loader'
+        ]
+      },
+      { test: /\.vue$/, use: 'vue-loader' },
+      { test: /\.js$/, use: 'babel-loader' }
+    ]
+  },
+  optimization: {
+    minimizer: [
+      new TerserPlugin({
+        cache: true,
+        parallel: true,
+        sourceMap: true,
+      }),
+    ],
+  },
+  plugins: [
+    new VueLoaderPlugin()
+  ]
+};
