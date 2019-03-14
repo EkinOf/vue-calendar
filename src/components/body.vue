372f5f6e8207d325cfbb8a6bc230cc4a571ee0a8
Kevin Desigaux <k.desigaux@cpro.fr>
2019-03-14 09:24:11 +0100

    changed cellIndex count

--- src/components/body.vue
+++ src/components/body.vue
@@ -211,7 +211,7 @@
         for (let i = 0;i<thisDayEvents.length;i++) {
           thisDayEvents[i].cellIndex = thisDayEvents[i].cellIndex || (i + 1)
           thisDayEvents[i].isShow = true
-          if (thisDayEvents[i].cellIndex == i+1 || i>2) continue
+          if (thisDayEvents[i].cellIndex == i+1 || i>3) continue
           thisDayEvents.splice(i,0,{
             title : 'holder',
             cellIndex : i+1,
